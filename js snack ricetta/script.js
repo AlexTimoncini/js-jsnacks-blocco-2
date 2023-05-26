3f745910c905decaa667177decbfbc67556dfda4
AlexTimoncini <alextimonci@gmail.com>
2023-05-26 12:53:08 +0200

    css + generatePlate

--- js snack ricetta/script.js
+++ js snack ricetta/script.js
@@ -5,10 +5,24 @@
 let ingredientList = document.getElementById('ingredients_list');
 let newIngredient = document.getElementById('ingredient');
 let addIngredientBtn = document.getElementById('addIngredient');
+let dishGenBtn = document.getElementById('dish');
+let ingredientCounter = 0;
+let dishImg = document.getElementById('dish_img');
 
 addIngredientBtn.addEventListener('click', function(){
     let li = document.createElement('li');
     li.append(newIngredient.value);
     ingredientList.appendChild(li);
+    ingredientCounter++;
+    newIngredient.value = '';
 });
 
+dishGenBtn.addEventListener('click', function(){
+    if (ingredientCounter >= 2){
+        setTimeout(generatePlate, (ingredientCounter * 2000), dishImg);
+    }
+});
+
+function generatePlate(dishImageParent){
+    dishImageParent.style.opacity = '1';
+}
