a7f71198a5dc742e6bf2cd000d8d44afa0f5fd6f
AlexTimoncini <alextimonci@gmail.com>
2023-05-29 11:09:20 +0200

    creted onject for basketaball player

---
+++ js-snack-oggetti/script.js
@@ -0,0 +1,21 @@
+// Creare un oggetto che rappresenti un giocatore di basket, con le seguenti proprietà:
+//     Codice giocatore
+//     Nome
+//     Cognome
+//     Età
+//     Media punti fatti per partita
+//     Percentuale di successo per tiri da 3 punti
+//     Stoppate 
+//     Tiri 
+
+
+const basketPlayer = {
+    code: '2x5we',
+    firstName: 'Bob',
+    lastName: 'Wayne',
+    age: 25,
+    scoreM: 30,
+    '3-point(%)': 15,
+    stop: 40,
+    shots: 70
+};
